14800a1fe28aca6794fe350576efd70761deb08a
red-old-hem <buddah93@gmail.com>
2026-03-23 22:15:28 +0900

    データベーススキーマを定義し、formsおよびsubmissionsテーブルを追加

--- app/.server/db/schema.ts
+++ app/.server/db/schema.ts
@@ -1,1 +1,17 @@
-export {}
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { defaultHex, defaultNow, foreign, id } from './helpers'
+
+export const forms = sqliteTable('forms', {
+  id: id(),
+  title: text().notNull(),
+  fields: text({ mode: 'json' }).notNull(),
+  token: defaultHex(),
+  created_at: defaultNow(),
+})
+
+export const submissions = sqliteTable('submissions', {
+  id: id(),
+  form_id: foreign(() => forms).notNull(),
+  data: text({ mode: 'json' }).notNull(),
+  created_at: defaultNow(),
+})
